faed9fd2d78ab5cdf82123ccac4cc81b65543f60
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-01-10 16:19:29 +0000

    refactor: add chat model

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+use macros_create_app::make_app65;
 use macros_make_error::make_error2;
+use macros_make_model::make_model22;
+use macros_make_scope::make_scope;
+use serde::*;
 
 make_error2!(ChatError);
+
+make_model22!(
+    QChat,
+    IChat,
+    OChat,
+    chat,
+    wallet_id: i32,
+    message: String,
+    room: i32,
+    wallet_id: i32
+);
