9e27e2cb392951bbff6b833f79a1b88060959682
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-01-11 01:23:48 +0000

    refactor: listen chat

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use macros_create_app::make_app65;
+use macros_create_app::make_app68;
 use macros_make_error::make_error2;
 use macros_make_model::make_model22;
 use macros_make_scope::make_scope;
@@ -16,20 +16,12 @@ make_model22!(
     wallet_id: i32
 );
 
-#[derive(
-    utoipa::ToSchema, Debug, PartialEq, serde::Deserialize, serde::Serialize, Clone, Default,
-)]
-pub struct ChatRequest {
-    pub data: IChat,
-    pub wallet_request: lib_auth_id::WalletAuthId,
-}
-
 #[derive(Debug, serde::Deserialize, utoipa::IntoParams)]
 struct IdPathParam {
     pub id: i32,
 }
 
-make_app65!(
+make_app68!(
     [message: String, room: i32, wallet_id: i32],
     chat_route,
     "/chat",
@@ -38,9 +30,10 @@ make_app65!(
     "{id}",
     OChat,
     QChat,
+    IChat,
+    ChatRequest,
     chat,
     [
-        ChatRequest,
         |s: actix_web::web::Data<my_state::MyState>,
          json: actix_web::web::Json<ChatRequest>,
          wallet: lib_wallet::QWallet,
@@ -56,30 +49,6 @@ async fn handle(
     json: actix_web::web::Json<ChatRequest>,
     _: lib_wallet::QWallet,
 ) -> Result<QChat, ChatError> {
-    tokio::spawn({
-        let s = s.clone();
-        let json = json.clone();
-        async move {
-            println!("hello {:?}", json.data);
-            s.req
-                .post(&format!("{}/queue/notification", s.env.cwa_2_api))
-                .json::<IChat>(&json.data)
-                .send()
-                .await
-                .map_err(ChatError::from_general)
-                .map_err(|x| {
-                    tracing::error!("{:?}", x);
-                    x
-                })?
-                .error_for_status()
-                .map_err(ChatError::from_general)
-                .map_err(|x| {
-                    tracing::error!("{:?}", x);
-                    x
-                })?;
-            Ok::<(), ChatError>(())
-        }
-    });
     chat::postgres_query::insert(&s.sqlx_pool, &json.data)
         .await
         .map_err(ChatError::from_general)
